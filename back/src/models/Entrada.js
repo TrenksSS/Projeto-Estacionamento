30066f2f6be0926b860061c2b32dde2eb9516f4a
RodolfoAura <rodolfo.d.s.aura@gmail.com>
2022-11-16 10:47:22 -0300

    valor add

--- back/src/models/Entrada.js
+++ back/src/models/Entrada.js
@@ -1,5 +1,5 @@
 const toCreateEntrada = (model) => {
-    return `INSERT INTO entradas VALUES (${model.id_cliente},'${model.placa_veiculo}','${model.id_vaga}'${model.hr_ent},${model.hr_sai})`;
+    return `INSERT INTO entradas VALUES (${model.id_cliente},'${model.placa_veiculo}','${model.id_vaga}'${model.hr_ent},${model.hr_sai}${model.valor})`;
 }
 
 const toReadAll = () => {
@@ -12,7 +12,8 @@ const toUpdate = (model) => {
     placa_veiculo = '${model.placa_veiculo}',
     id_vaga = ${model.id_vaga},
     hr_ent = '${model.hr_ent}',
-    hr_sai = ${model.hr_sai}`;
+    hr_sai = ${model.hr_sai},
+    valor = '${model.valor}'`;
 }
 
 
